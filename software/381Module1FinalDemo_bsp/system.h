b7fa3692c7468f97421bb18a26a9c1024c34d025
lomash <lomashgupta@gmail.com>
2014-10-17 22:40:52 -0700

    final demo module 1
    clean project with all the source code

---
+++ software/381Module1FinalDemo_bsp/system.h
@@ -0,0 +1,552 @@
+/*
+ * system.h - SOPC Builder system and BSP software package information
+ *
+ * Machine generated for CPU 'nios2_system' in SOPC Builder design 'nios2_system'
+ * SOPC Builder design path: E:/Downloads/CannonMan_restored/nios2_system.sopcinfo
+ *
+ * Generated: Thu Oct 16 15:17:43 PDT 2014
+ */
+
+/*
+ * DO NOT MODIFY THIS FILE
+ *
+ * Changing this file will have subtle consequences
+ * which will almost certainly lead to a nonfunctioning
+ * system. If you do modify this file, be aware that your
+ * changes will be overwritten and lost when this file
+ * is generated again.
+ *
+ * DO NOT MODIFY THIS FILE
+ */
+
+/*
+ * License Agreement
+ *
+ * Copyright (c) 2008
+ * Altera Corporation, San Jose, California, USA.
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ * This agreement shall be governed in all respects by the laws of the State
+ * of California and by the laws of the United States of America.
+ */
+
+#ifndef __SYSTEM_H_
+#define __SYSTEM_H_
+
+/* Include definitions from linker script generator */
+#include "linker.h"
+
+
+/*
+ * Altera_UP_SD_Card_Avalon_Interface_0 configuration
+ *
+ */
+
+#define ALTERA_UP_SD_CARD_AVALON_INTERFACE_0_BASE 0x4400
+#define ALTERA_UP_SD_CARD_AVALON_INTERFACE_0_IRQ -1
+#define ALTERA_UP_SD_CARD_AVALON_INTERFACE_0_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define ALTERA_UP_SD_CARD_AVALON_INTERFACE_0_NAME "/dev/Altera_UP_SD_Card_Avalon_Interface_0"
+#define ALTERA_UP_SD_CARD_AVALON_INTERFACE_0_SPAN 1024
+#define ALTERA_UP_SD_CARD_AVALON_INTERFACE_0_TYPE "Altera_UP_SD_Card_Avalon_Interface"
+#define ALT_MODULE_CLASS_Altera_UP_SD_Card_Avalon_Interface_0 Altera_UP_SD_Card_Avalon_Interface
+
+
+/*
+ * CPU configuration
+ *
+ */
+
+#define ALT_CPU_ARCHITECTURE "altera_nios2_qsys"
+#define ALT_CPU_BIG_ENDIAN 0
+#define ALT_CPU_BREAK_ADDR 0x1820
+#define ALT_CPU_CPU_FREQ 50000000u
+#define ALT_CPU_CPU_ID_SIZE 1
+#define ALT_CPU_CPU_ID_VALUE 0x00000000
+#define ALT_CPU_CPU_IMPLEMENTATION "fast"
+#define ALT_CPU_DATA_ADDR_WIDTH 0x19
+#define ALT_CPU_DCACHE_LINE_SIZE 32
+#define ALT_CPU_DCACHE_LINE_SIZE_LOG2 5
+#define ALT_CPU_DCACHE_SIZE 2048
+#define ALT_CPU_EXCEPTION_ADDR 0x1000020
+#define ALT_CPU_FLUSHDA_SUPPORTED
+#define ALT_CPU_FREQ 50000000
+#define ALT_CPU_HARDWARE_DIVIDE_PRESENT 0
+#define ALT_CPU_HARDWARE_MULTIPLY_PRESENT 1
+#define ALT_CPU_HARDWARE_MULX_PRESENT 0
+#define ALT_CPU_HAS_DEBUG_CORE 1
+#define ALT_CPU_HAS_DEBUG_STUB
+#define ALT_CPU_HAS_JMPI_INSTRUCTION
+#define ALT_CPU_ICACHE_LINE_SIZE 32
+#define ALT_CPU_ICACHE_LINE_SIZE_LOG2 5
+#define ALT_CPU_ICACHE_SIZE 4096
+#define ALT_CPU_INITDA_SUPPORTED
+#define ALT_CPU_INST_ADDR_WIDTH 0x19
+#define ALT_CPU_NAME "nios2_system"
+#define ALT_CPU_NUM_OF_SHADOW_REG_SETS 0
+#define ALT_CPU_RESET_ADDR 0x1000000
+
+
+/*
+ * CPU configuration (with legacy prefix - don't use these anymore)
+ *
+ */
+
+#define NIOS2_BIG_ENDIAN 0
+#define NIOS2_BREAK_ADDR 0x1820
+#define NIOS2_CPU_FREQ 50000000u
+#define NIOS2_CPU_ID_SIZE 1
+#define NIOS2_CPU_ID_VALUE 0x00000000
+#define NIOS2_CPU_IMPLEMENTATION "fast"
+#define NIOS2_DATA_ADDR_WIDTH 0x19
+#define NIOS2_DCACHE_LINE_SIZE 32
+#define NIOS2_DCACHE_LINE_SIZE_LOG2 5
+#define NIOS2_DCACHE_SIZE 2048
+#define NIOS2_EXCEPTION_ADDR 0x1000020
+#define NIOS2_FLUSHDA_SUPPORTED
+#define NIOS2_HARDWARE_DIVIDE_PRESENT 0
+#define NIOS2_HARDWARE_MULTIPLY_PRESENT 1
+#define NIOS2_HARDWARE_MULX_PRESENT 0
+#define NIOS2_HAS_DEBUG_CORE 1
+#define NIOS2_HAS_DEBUG_STUB
+#define NIOS2_HAS_JMPI_INSTRUCTION
+#define NIOS2_ICACHE_LINE_SIZE 32
+#define NIOS2_ICACHE_LINE_SIZE_LOG2 5
+#define NIOS2_ICACHE_SIZE 4096
+#define NIOS2_INITDA_SUPPORTED
+#define NIOS2_INST_ADDR_WIDTH 0x19
+#define NIOS2_NUM_OF_SHADOW_REG_SETS 0
+#define NIOS2_RESET_ADDR 0x1000000
+
+
+/*
+ * Define for each module class mastered by the CPU
+ *
+ */
+
+#define __ALTERA_AVALON_JTAG_UART
+#define __ALTERA_AVALON_NEW_SDRAM_CONTROLLER
+#define __ALTERA_AVALON_ONCHIP_MEMORY2
+#define __ALTERA_AVALON_PIO
+#define __ALTERA_AVALON_TIMER
+#define __ALTERA_NIOS2_QSYS
+#define __ALTERA_UP_AVALON_AUDIO
+#define __ALTERA_UP_AVALON_AUDIO_AND_VIDEO_CONFIG
+#define __ALTERA_UP_AVALON_CHARACTER_LCD
+#define __ALTERA_UP_AVALON_PS2
+#define __ALTERA_UP_AVALON_SRAM
+#define __ALTERA_UP_AVALON_VIDEO_CHARACTER_BUFFER_WITH_DMA
+#define __ALTERA_UP_AVALON_VIDEO_PIXEL_BUFFER_DMA
+#define __ALTERA_UP_SD_CARD_AVALON_INTERFACE
+#define __PIXEL_DRAWER
+
+
+/*
+ * JTAG configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_JTAG altera_avalon_jtag_uart
+#define JTAG_BASE 0x4938
+#define JTAG_IRQ 0
+#define JTAG_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define JTAG_NAME "/dev/JTAG"
+#define JTAG_READ_DEPTH 64
+#define JTAG_READ_THRESHOLD 8
+#define JTAG_SPAN 8
+#define JTAG_TYPE "altera_avalon_jtag_uart"
+#define JTAG_WRITE_DEPTH 64
+#define JTAG_WRITE_THRESHOLD 8
+
+
+/*
+ * LEDG configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_LEDG altera_avalon_pio
+#define LEDG_BASE 0x4910
+#define LEDG_BIT_CLEARING_EDGE_REGISTER 0
+#define LEDG_BIT_MODIFYING_OUTPUT_REGISTER 0
+#define LEDG_CAPTURE 0
+#define LEDG_DATA_WIDTH 8
+#define LEDG_DO_TEST_BENCH_WIRING 0
+#define LEDG_DRIVEN_SIM_VALUE 0
+#define LEDG_EDGE_TYPE "NONE"
+#define LEDG_FREQ 50000000
+#define LEDG_HAS_IN 0
+#define LEDG_HAS_OUT 1
+#define LEDG_HAS_TRI 0
+#define LEDG_IRQ -1
+#define LEDG_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define LEDG_IRQ_TYPE "NONE"
+#define LEDG_NAME "/dev/LEDG"
+#define LEDG_RESET_VALUE 0
+#define LEDG_SPAN 16
+#define LEDG_TYPE "altera_avalon_pio"
+
+
+/*
+ * LEDR configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_LEDR altera_avalon_pio
+#define LEDR_BASE 0x4900
+#define LEDR_BIT_CLEARING_EDGE_REGISTER 0
+#define LEDR_BIT_MODIFYING_OUTPUT_REGISTER 0
+#define LEDR_CAPTURE 0
+#define LEDR_DATA_WIDTH 18
+#define LEDR_DO_TEST_BENCH_WIRING 0
+#define LEDR_DRIVEN_SIM_VALUE 0
+#define LEDR_EDGE_TYPE "NONE"
+#define LEDR_FREQ 50000000
+#define LEDR_HAS_IN 0
+#define LEDR_HAS_OUT 1
+#define LEDR_HAS_TRI 0
+#define LEDR_IRQ -1
+#define LEDR_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define LEDR_IRQ_TYPE "NONE"
+#define LEDR_NAME "/dev/LEDR"
+#define LEDR_RESET_VALUE 0
+#define LEDR_SPAN 16
+#define LEDR_TYPE "altera_avalon_pio"
+
+
+/*
+ * System configuration
+ *
+ */
+
+#define ALT_DEVICE_FAMILY "Cyclone II"
+#define ALT_IRQ_BASE NULL
+#define ALT_LEGACY_INTERRUPT_API_PRESENT
+#define ALT_LOG_PORT "/dev/null"
+#define ALT_LOG_PORT_BASE 0x0
+#define ALT_LOG_PORT_DEV null
+#define ALT_LOG_PORT_TYPE ""
+#define ALT_NUM_EXTERNAL_INTERRUPT_CONTROLLERS 0
+#define ALT_NUM_INTERNAL_INTERRUPT_CONTROLLERS 1
+#define ALT_NUM_INTERRUPT_CONTROLLERS 1
+#define ALT_STDERR "/dev/JTAG"
+#define ALT_STDERR_BASE 0x4938
+#define ALT_STDERR_DEV JTAG
+#define ALT_STDERR_IS_JTAG_UART
+#define ALT_STDERR_PRESENT
+#define ALT_STDERR_TYPE "altera_avalon_jtag_uart"
+#define ALT_STDIN "/dev/JTAG"
+#define ALT_STDIN_BASE 0x4938
+#define ALT_STDIN_DEV JTAG
+#define ALT_STDIN_IS_JTAG_UART
+#define ALT_STDIN_PRESENT
+#define ALT_STDIN_TYPE "altera_avalon_jtag_uart"
+#define ALT_STDOUT "/dev/JTAG"
+#define ALT_STDOUT_BASE 0x4938
+#define ALT_STDOUT_DEV JTAG
+#define ALT_STDOUT_IS_JTAG_UART
+#define ALT_STDOUT_PRESENT
+#define ALT_STDOUT_TYPE "altera_avalon_jtag_uart"
+#define ALT_SYSTEM_NAME "nios2_system"
+
+
+/*
+ * audio_0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_audio_0 altera_up_avalon_audio
+#define AUDIO_0_BASE 0x48c0
+#define AUDIO_0_IRQ 3
+#define AUDIO_0_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define AUDIO_0_NAME "/dev/audio_0"
+#define AUDIO_0_SPAN 16
+#define AUDIO_0_TYPE "altera_up_avalon_audio"
+
+
+/*
+ * audio_and_video_config_0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_audio_and_video_config_0 altera_up_avalon_audio_and_video_config
+#define AUDIO_AND_VIDEO_CONFIG_0_BASE 0x48b0
+#define AUDIO_AND_VIDEO_CONFIG_0_IRQ -1
+#define AUDIO_AND_VIDEO_CONFIG_0_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define AUDIO_AND_VIDEO_CONFIG_0_NAME "/dev/audio_and_video_config_0"
+#define AUDIO_AND_VIDEO_CONFIG_0_SPAN 16
+#define AUDIO_AND_VIDEO_CONFIG_0_TYPE "altera_up_avalon_audio_and_video_config"
+
+
+/*
+ * character_lcd_0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_character_lcd_0 altera_up_avalon_character_lcd
+#define CHARACTER_LCD_0_BASE 0x4940
+#define CHARACTER_LCD_0_IRQ -1
+#define CHARACTER_LCD_0_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define CHARACTER_LCD_0_NAME "/dev/character_lcd_0"
+#define CHARACTER_LCD_0_SPAN 2
+#define CHARACTER_LCD_0_TYPE "altera_up_avalon_character_lcd"
+
+
+/*
+ * hal configuration
+ *
+ */
+
+#define ALT_MAX_FD 32
+#define ALT_SYS_CLK none
+#define ALT_TIMESTAMP_CLK SDCARD_TIMER
+
+
+/*
+ * key configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_key altera_avalon_pio
+#define KEY_BASE 0x48e0
+#define KEY_BIT_CLEARING_EDGE_REGISTER 0
+#define KEY_BIT_MODIFYING_OUTPUT_REGISTER 0
+#define KEY_CAPTURE 0
+#define KEY_DATA_WIDTH 4
+#define KEY_DO_TEST_BENCH_WIRING 0
+#define KEY_DRIVEN_SIM_VALUE 0
+#define KEY_EDGE_TYPE "NONE"
+#define KEY_FREQ 50000000
+#define KEY_HAS_IN 1
+#define KEY_HAS_OUT 0
+#define KEY_HAS_TRI 0
+#define KEY_IRQ -1
+#define KEY_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define KEY_IRQ_TYPE "NONE"
+#define KEY_NAME "/dev/key"
+#define KEY_RESET_VALUE 0
+#define KEY_SPAN 16
+#define KEY_TYPE "altera_avalon_pio"
+
+
+/*
+ * onchip configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_onchip altera_avalon_onchip_memory2
+#define ONCHIP_ALLOW_IN_SYSTEM_MEMORY_CONTENT_EDITOR 0
+#define ONCHIP_ALLOW_MRAM_SIM_CONTENTS_ONLY_FILE 0
+#define ONCHIP_BASE 0x0
+#define ONCHIP_CONTENTS_INFO ""
+#define ONCHIP_DUAL_PORT 0
+#define ONCHIP_GUI_RAM_BLOCK_TYPE "AUTO"
+#define ONCHIP_INIT_CONTENTS_FILE "nios2_system_onchip"
+#define ONCHIP_INIT_MEM_CONTENT 1
+#define ONCHIP_INSTANCE_ID "NONE"
+#define ONCHIP_IRQ -1
+#define ONCHIP_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define ONCHIP_NAME "/dev/onchip"
+#define ONCHIP_NON_DEFAULT_INIT_FILE_ENABLED 0
+#define ONCHIP_RAM_BLOCK_TYPE "AUTO"
+#define ONCHIP_READ_DURING_WRITE_MODE "DONT_CARE"
+#define ONCHIP_SINGLE_CLOCK_OP 0
+#define ONCHIP_SIZE_MULTIPLE 1
+#define ONCHIP_SIZE_VALUE 4096
+#define ONCHIP_SPAN 4096
+#define ONCHIP_TYPE "altera_avalon_onchip_memory2"
+#define ONCHIP_WRITABLE 1
+
+
+/*
+ * pixel_drawer_0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_pixel_drawer_0 pixel_drawer
+#define PIXEL_DRAWER_0_BASE 0x4820
+#define PIXEL_DRAWER_0_IRQ -1
+#define PIXEL_DRAWER_0_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define PIXEL_DRAWER_0_NAME "/dev/pixel_drawer_0"
+#define PIXEL_DRAWER_0_SPAN 32
+#define PIXEL_DRAWER_0_TYPE "pixel_drawer"
+
+
+/*
+ * ps2_0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_ps2_0 altera_up_avalon_ps2
+#define PS2_0_BASE 0x4928
+#define PS2_0_IRQ 2
+#define PS2_0_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define PS2_0_NAME "/dev/ps2_0"
+#define PS2_0_SPAN 8
+#define PS2_0_TYPE "altera_up_avalon_ps2"
+
+
+/*
+ * sdcard_timer configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_sdcard_timer altera_avalon_timer
+#define SDCARD_TIMER_ALWAYS_RUN 0
+#define SDCARD_TIMER_BASE 0x4840
+#define SDCARD_TIMER_COUNTER_SIZE 32
+#define SDCARD_TIMER_FIXED_PERIOD 0
+#define SDCARD_TIMER_FREQ 50000000
+#define SDCARD_TIMER_IRQ 1
+#define SDCARD_TIMER_IRQ_INTERRUPT_CONTROLLER_ID 0
+#define SDCARD_TIMER_LOAD_VALUE 49999
+#define SDCARD_TIMER_MULT 0.0010
+#define SDCARD_TIMER_NAME "/dev/sdcard_timer"
+#define SDCARD_TIMER_PERIOD 1
+#define SDCARD_TIMER_PERIOD_UNITS "ms"
+#define SDCARD_TIMER_RESET_OUTPUT 0
+#define SDCARD_TIMER_SNAPSHOT 1
+#define SDCARD_TIMER_SPAN 32
+#define SDCARD_TIMER_TICKS_PER_SEC 1000.0
+#define SDCARD_TIMER_TIMEOUT_PULSE_OUTPUT 0
+#define SDCARD_TIMER_TYPE "altera_avalon_timer"
+
+
+/*
+ * sdram configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_sdram altera_avalon_new_sdram_controller
+#define SDRAM_BASE 0x1000000
+#define SDRAM_CAS_LATENCY 3
+#define SDRAM_CONTENTS_INFO
+#define SDRAM_INIT_NOP_DELAY 0.0
+#define SDRAM_INIT_REFRESH_COMMANDS 2
+#define SDRAM_IRQ -1
+#define SDRAM_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define SDRAM_IS_INITIALIZED 1
+#define SDRAM_NAME "/dev/sdram"
+#define SDRAM_POWERUP_DELAY 100.0
+#define SDRAM_REFRESH_PERIOD 15.625
+#define SDRAM_REGISTER_DATA_IN 1
+#define SDRAM_SDRAM_ADDR_WIDTH 0x16
+#define SDRAM_SDRAM_BANK_WIDTH 2
+#define SDRAM_SDRAM_COL_WIDTH 8
+#define SDRAM_SDRAM_DATA_WIDTH 16
+#define SDRAM_SDRAM_NUM_BANKS 4
+#define SDRAM_SDRAM_NUM_CHIPSELECTS 1
+#define SDRAM_SDRAM_ROW_WIDTH 12
+#define SDRAM_SHARED_DATA 0
+#define SDRAM_SIM_MODEL_BASE 0
+#define SDRAM_SPAN 8388608
+#define SDRAM_STARVATION_INDICATOR 0
+#define SDRAM_TRISTATE_BRIDGE_SLAVE ""
+#define SDRAM_TYPE "altera_avalon_new_sdram_controller"
+#define SDRAM_T_AC 5.5
+#define SDRAM_T_MRD 3
+#define SDRAM_T_RCD 20.0
+#define SDRAM_T_RFC 70.0
+#define SDRAM_T_RP 20.0
+#define SDRAM_T_WR 14.0
+
+
+/*
+ * sram_0 configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_sram_0 altera_up_avalon_sram
+#define SRAM_0_BASE 0x80000
+#define SRAM_0_IRQ -1
+#define SRAM_0_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define SRAM_0_NAME "/dev/sram_0"
+#define SRAM_0_SPAN 524288
+#define SRAM_0_TYPE "altera_up_avalon_sram"
+
+
+/*
+ * switches configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_switches altera_avalon_pio
+#define SWITCHES_BASE 0x48f0
+#define SWITCHES_BIT_CLEARING_EDGE_REGISTER 0
+#define SWITCHES_BIT_MODIFYING_OUTPUT_REGISTER 0
+#define SWITCHES_CAPTURE 0
+#define SWITCHES_DATA_WIDTH 18
+#define SWITCHES_DO_TEST_BENCH_WIRING 0
+#define SWITCHES_DRIVEN_SIM_VALUE 0
+#define SWITCHES_EDGE_TYPE "NONE"
+#define SWITCHES_FREQ 50000000
+#define SWITCHES_HAS_IN 1
+#define SWITCHES_HAS_OUT 0
+#define SWITCHES_HAS_TRI 0
+#define SWITCHES_IRQ -1
+#define SWITCHES_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define SWITCHES_IRQ_TYPE "NONE"
+#define SWITCHES_NAME "/dev/switches"
+#define SWITCHES_RESET_VALUE 0
+#define SWITCHES_SPAN 16
+#define SWITCHES_TYPE "altera_avalon_pio"
+
+
+/*
+ * video_character_buffer_with_dma_0_avalon_char_buffer_slave configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_video_character_buffer_with_dma_0_avalon_char_buffer_slave altera_up_avalon_video_character_buffer_with_dma
+#define VIDEO_CHARACTER_BUFFER_WITH_DMA_0_AVALON_CHAR_BUFFER_SLAVE_BASE 0x2000
+#define VIDEO_CHARACTER_BUFFER_WITH_DMA_0_AVALON_CHAR_BUFFER_SLAVE_IRQ -1
+#define VIDEO_CHARACTER_BUFFER_WITH_DMA_0_AVALON_CHAR_BUFFER_SLAVE_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define VIDEO_CHARACTER_BUFFER_WITH_DMA_0_AVALON_CHAR_BUFFER_SLAVE_NAME "/dev/video_character_buffer_with_dma_0_avalon_char_buffer_slave"
+#define VIDEO_CHARACTER_BUFFER_WITH_DMA_0_AVALON_CHAR_BUFFER_SLAVE_SPAN 8192
+#define VIDEO_CHARACTER_BUFFER_WITH_DMA_0_AVALON_CHAR_BUFFER_SLAVE_TYPE "altera_up_avalon_video_character_buffer_with_dma"
+
+
+/*
+ * video_character_buffer_with_dma_0_avalon_char_control_slave configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_video_character_buffer_with_dma_0_avalon_char_control_slave altera_up_avalon_video_character_buffer_with_dma
+#define VIDEO_CHARACTER_BUFFER_WITH_DMA_0_AVALON_CHAR_CONTROL_SLAVE_BASE 0x4930
+#define VIDEO_CHARACTER_BUFFER_WITH_DMA_0_AVALON_CHAR_CONTROL_SLAVE_IRQ -1
+#define VIDEO_CHARACTER_BUFFER_WITH_DMA_0_AVALON_CHAR_CONTROL_SLAVE_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define VIDEO_CHARACTER_BUFFER_WITH_DMA_0_AVALON_CHAR_CONTROL_SLAVE_NAME "/dev/video_character_buffer_with_dma_0_avalon_char_control_slave"
+#define VIDEO_CHARACTER_BUFFER_WITH_DMA_0_AVALON_CHAR_CONTROL_SLAVE_SPAN 8
+#define VIDEO_CHARACTER_BUFFER_WITH_DMA_0_AVALON_CHAR_CONTROL_SLAVE_TYPE "altera_up_avalon_video_character_buffer_with_dma"
+
+
+/*
+ * video_pixel_buffer_dma configuration
+ *
+ */
+
+#define ALT_MODULE_CLASS_video_pixel_buffer_dma altera_up_avalon_video_pixel_buffer_dma
+#define VIDEO_PIXEL_BUFFER_DMA_BASE 0x48d0
+#define VIDEO_PIXEL_BUFFER_DMA_IRQ -1
+#define VIDEO_PIXEL_BUFFER_DMA_IRQ_INTERRUPT_CONTROLLER_ID -1
+#define VIDEO_PIXEL_BUFFER_DMA_NAME "/dev/video_pixel_buffer_dma"
+#define VIDEO_PIXEL_BUFFER_DMA_SPAN 16
+#define VIDEO_PIXEL_BUFFER_DMA_TYPE "altera_up_avalon_video_pixel_buffer_dma"
+
+#endif /* __SYSTEM_H_ */
